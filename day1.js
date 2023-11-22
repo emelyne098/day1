e2a48c3c247aba8fe29061c70bdd8450b30eefcf
IRADUKUNDA <iradukundemelyne09@gmail.com>
2023-11-22 16:07:25 +0200

    update

---
+++ day1.js
@@ -0,0 +1,22 @@
+console.log(1);
+let test;1
+test=1
+console.log(test)
+let num=3;
+console.log(num)
+num=4
+console.log(num)
+let numberone=7
+numbertwo=numberone
+console.log(numbertwo)
+const numb= 8;
+console.log(numb)
+
+
+
+
+
+
+
+
+    0
